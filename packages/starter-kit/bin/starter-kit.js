afee367c1b9a3ce74c1065523822135ba030c73d
Tim Spears <timspears@imarc.com>
2023-07-19 09:47:31 -0400

    fix dir path

--- packages/starter-kit/bin/starter-kit.js
+++ packages/starter-kit/bin/starter-kit.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-const src = path.resolve(__dirname)
+const src = path.join(__dirname, '..')
 const dest = path.resolve('./')
 const packageJsonPath = path.resolve('./package.json')
 
