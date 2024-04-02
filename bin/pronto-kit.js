bba86e5c0bbd578e500bf45d055b89a2de563647
Tristan Norton <tristan.m.norton@gmail.com>
2024-04-02 14:56:07 -0400

    Delete bin config after pulling with npx command.

--- bin/pronto-kit.js
+++ bin/pronto-kit.js
@@ -2,6 +2,7 @@
 import fs from 'fs-extra'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import { execSync } from 'child_process'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -15,11 +16,14 @@ const files = [
     'assets',
     'vite.config.js',
     'package.json',
+    'main-icons-sprite.svg',
 ]
 
 files.forEach(file => {
     fs.copySync(path.join(src, file), path.join(dest, file), { overwrite: false })
 })
+
+execSync('npm pkg delete scripts.bin')
 
 console.log('Done! 🚀')
 console.log('Run `npm install` to get started.')
