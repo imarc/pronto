e60da873b2324c5261adc28b00df1a6fb7cf4b47
Tristan Norton <tristan.m.norton@gmail.com>
2024-04-02 14:47:48 -0400

    Variable Scope Issue

--- bin/pronto-kit.js
+++ bin/pronto-kit.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 import fs from 'fs-extra'
 import path from 'path'
+import { fileURLToPath } from 'url'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const src = path.join(__dirname, '..')
 const dest = path.resolve('./')
 
