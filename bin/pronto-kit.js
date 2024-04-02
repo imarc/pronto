451deeb5f58133c6f90e62ee6001a223b7061cb1
Tristan Norton <tristan.m.norton@gmail.com>
2024-04-02 14:45:13 -0400

    Fixed node issue with bin file.

--- bin/pronto-kit.js
+++ bin/pronto-kit.js
@@ -1,5 +1,6 @@
-const fs = require('fs-extra')
-const path = require('path')
+#!/usr/bin/env node
+import fs from 'fs-extra'
+import path from 'path'
 
 const src = path.join(__dirname, '..')
 const dest = path.resolve('./')
