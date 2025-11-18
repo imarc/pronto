3077e93efcb6417c095f059c09b719702aa17889
Kevin Hamer <kevin@imarc.com>
2025-11-18 17:09:13 -0500

    implement changes based on copilot PR review

--- resources/js/components/PAccordion.vue
+++ resources/js/components/PAccordion.vue
@@ -1,26 +1,21 @@
 <script setup>
-import { onMounted, useTemplateRef } from 'vue'
+import { useTemplateRef } from 'vue'
 
 /**
  * PAccordion is a vue component for the <details> element. It uses the native <details> element
  * state to determine whether it's open or closed, and merely handles updating CSS properties
  * to allow the element to use transitions between dynamic closed and [open] heights.
  * 
- * @param {number} duration - Duration of the animation in milliseconds.
- *                            Default: computed transition-duration of the details element.
  * @param {string} summary - selector for the summary element.
  *                           Default: 'summary:first-of-type'
  */
-
 const props = defineProps({
-  duration: { type: Number },
   summary: { type: String, default: 'summary:first-of-type' }
 })
 const details = useTemplateRef('details')
 
 const handleToggle = event => {
   const el = event.currentTarget
-  const duration = props.duration || parseFloat(getComputedStyle(el).transitionDuration) * 1000 || 0
   const summary = el.querySelector(props.summary)
 
   if (!summary) {
@@ -41,7 +36,7 @@ const handleToggle = event => {
     requestAnimationFrame(() => {
       const endingHeight = el.clientHeight
 
-      el.style.removeProperty('--accordion-transition-duration', 0)
+      el.style.removeProperty('--accordion-transition-duration')
       el.style.setProperty('--accordion-height', 0)
 
       requestAnimationFrame(() => {
